7d96001068bd0855ea3fee3b9fbdc3f4e3f3a283
Miguel <sonegs@hotmail.com>
2020-07-29 17:42:49 +0200

    First changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-fetch("https://www.breakingbadapi.com/api/").then(response => {
-    console.log(response);
-});
+//fetch("https://www.breakingbadapi.com/api/")
+fetch("https://api.github.com/orgs/apple/members")
+    .then(response => {
+        return response.json();
+    }).then(data => {
+        for (var item of data) {
+            console.log(item.login);
+        }
+    }).catch(error => {
+        document.write("Se ha producido un error en la llamada a la API");
+    });
